42deca67a2ffe02189623181199c8862626b0d1a
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-03-05 18:01:17 +0530

    update

--- vm-create.sh
+++ vm-create.sh
@@ -5,8 +5,8 @@ catalogue user cart payment shipping
 user cart payment shipping frontend mongodb redis mysql rabbitmq dispatch
 
 for component in vmimage ; do
-az vm create --resource-group azure-training-2023 --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name azure-training-2023-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-address "" --size Standard_B1s --nsg "" 
-az vm auto-shutdown -g azure-training-2023 -n  $component --time 1230  
+        az vm create --resource-group azure-training-2023 --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name azure-training-2023-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-address "" --size Standard_B1s --nsg "" 
+        az vm auto-shutdown -g azure-training-2023 -n  $component --time 1230  
 done
 
 
